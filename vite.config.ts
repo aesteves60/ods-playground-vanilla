a0cd856da40ac7b64a4634c3ec3cda6bd7a0d69c
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-09-13 11:18:55 +0200

    fix: vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'ods-playground-vanilla/',
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, './src/app'),
